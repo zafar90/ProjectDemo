891ddfd92cc158a678e6ffcd14dd4a1b78b546ca
zafar <fsauser@fstack.com>
2025-01-25 04:15:37 +0000

    updated code

--- src/main/java/com/example/bankapp/BankappApplication.java
+++ src/main/java/com/example/bankapp/BankappApplication.java
@@ -8,15 +8,7 @@ public class BankappApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BankappApplication.class, args);
-		
-		String nullString = null; // This will throw a NullPointerException
-                System.out.println(nullString.length());
 
-		String password = "1234";
-		String newpassword = "abcd";
-		String Onemorenewpassword = "cdafdag";
-		System.out.println("newpassword" + newpassword);
-                System.out.println("Password" + password);
 	}
 
 }
